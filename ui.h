bdbf3e28aa03b9c0f481c362ecbc8e73668f99d6
miaotian <miaotianshun@gmail.com>
2018-05-21 16:20:16 -0400

    finished loading the ct scan into opengl render window

--- ui.h
+++ ui.h
@@ -4,11 +4,20 @@
 
 // VTK header files
 #include <vtkPNGReader.h>
-#include <vtkSmartPointer.h>
-#include <vtkImageViewer2.h>
 #include <vtkActor.h>
 #include <vtkImageMapToWindowLevelColors.h>
 #include <vtkImageData.h>
+#include <vtkDICOMImageReader.h>
+#include <QVTKOpenGLWidget.h>
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkSmartVolumeMapper.h>
+#include <vtkNew.h>
+#include <vtkVolumeProperty.h>
+#include <vtkVolume.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkColorTransferFunction.h>
+#include <vtkPiecewiseFunction.h>
 
 // ITK header files
 #include <itkImage.h>
@@ -17,11 +26,14 @@
 #include <itkScalarImageKmeansImageFilter.h>
 
 // Qt header files
+#include <QPointer>
 #include <QFileDialog.h>
 #include <QMainWindow.h>
 #include <QLayout.h>
 #include <QPushButton.h>
 #include <QVTKWidget.h>
+#include <QSlider>
+#include <QLabel>
 
 // OpenCV header files
 #include <opencv2/core.hpp>
@@ -37,10 +49,24 @@ class ui : public QMainWindow
 {
 	Q_OBJECT
 private:
-	
+	// Declaration of  CLASS VARIABLES HERE
+	QPushButton * but1;
+	QSlider *sl1;
+	QLabel *label1;
+	QVBoxLayout *layout_vertical;
+	QHBoxLayout *layout_horizontal;
+	QPointer<QVTKOpenGLWidget> viewport;
+	vtkNew<vtkDICOMImageReader> reader;
+	vtkNew<vtkGenericOpenGLRenderWindow> window1;
+	QWidget *widget;
+	vtkNew<vtkSmartVolumeMapper> volumeMapper;
+	vtkNew<vtkVolumeProperty> volumeProperty;
+	vtkNew<vtkVolume> vol;
+	vtkNew<vtkRenderer> ren1;
+	vtkNew<vtkColorTransferFunction> color;
+	vtkNew<vtkPiecewiseFunction> compositeOpacity;
 public:
-	// CLASS VARIABLES HERE
-
+	
 	// Constructor (happens when created)
 	ui()
 	{
@@ -53,19 +79,56 @@ public:
 
 		// Create your widgets
 		// YOUR CODE HERE
-		
+		vtkOpenGLRenderWindow::SetGlobalMaximumNumberOfMultiSamples(0);
+		QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+		viewport = new QVTKOpenGLWidget();
+		viewport->SetRenderWindow(window1.Get());
+		volumeProperty->ShadeOff();
+		volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
+		compositeOpacity->AddPoint(0.0, 0.0);
+		compositeOpacity->AddPoint(80.0, 1.0);
+		compositeOpacity->AddPoint(80.1, 0.0);
+		compositeOpacity->AddPoint(255.0, 0.0);
+		volumeProperty->SetScalarOpacity(compositeOpacity);
+		color->AddRGBPoint(0.0, 0.0, 0.0, 1.0);
+		color->AddRGBPoint(40.0, 1.0, 0.0, 0.0);
+		color->AddRGBPoint(255.0, 1.0, 1.0, 1.0);
+		volumeProperty->SetColor(color);
+		but1 = new QPushButton("Load Data");
+		but1->setFixedSize(100, 30);
 		// Layout the widgets
 		// YOUR CODE HERE
-
+		layout_vertical = new QVBoxLayout();
+		widget->setLayout(layout_vertical);
+		layout_vertical->addWidget(viewport);
+		layout_vertical->addWidget(but1);
 		// Connected widget signals to slots
 		// YOUR CODE HERE
-
+		connect(but1, SIGNAL(released()), this, SLOT(load_data()));
 		// Display the window
 		this->show();
+	
 	}
 
 public slots: // This tells Qt we are defining slots here
 
 	// YOUR CODE HERE
+	void load_data()
+	{
+		// read all the dicom files in the specific directory
 
+		reader->SetDirectoryName("C:/Users/DoseOptics/Desktop/engs_199_final_project/data/d14068");
+		reader->Update();
+		// Image viewer
+		volumeMapper->SetInputConnection(reader->GetOutputPort());
+		volumeMapper->SetRequestedRenderModeToRayCast();
+		vol->SetMapper(volumeMapper);
+		vol->SetProperty(volumeProperty);
+		ren1->AddViewProp(vol);
+		ren1->ResetCamera();
+		viewport->GetRenderWindow()->AddRenderer(ren1);
+		viewport->GetRenderWindow()->Render();
+		
+		// set the slider minimum and maximum
+	}
 };
