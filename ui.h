a136f0d67b04f994402f083a9b3d68884b567bdd
miaotian <miaotianshun@gmail.com>
2018-05-23 16:52:28 -0400

    adding comments.

--- ui.h
+++ ui.h
@@ -98,12 +98,13 @@ public:
 		this->setCentralWidget(widget);
 
 		// Create your widgets
-		// YOUR CODE HERE
+		// Configuration of vtkopegl window
 		vtkOpenGLRenderWindow::SetGlobalMaximumNumberOfMultiSamples(0);
 		QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
 		viewport = new QVTKOpenGLWidget();
 		viewport->SetRenderWindow(window1.Get());
 		viewport->GetRenderWindow()->SetSize(1920, 1200);
+		// configuration of button etc. 
 		but1 = new QPushButton("Load CT");
 		but1->setFixedSize(100, 30);
 		but2 = new QPushButton("Load PNG");
@@ -117,6 +118,7 @@ public:
 		layout_vertical->addLayout(layout_horizontal);
 		layout_horizontal->addWidget(but1);
 		layout_horizontal->addWidget(but2);
+		// Configuration of the camera
 		cam1->SetPosition(-1247.36, -1048.52, -654.272);
 		cam1->SetFocalPoint(-12.5727, 11.141, 5.87349);
 		cam1->SetViewUp(0.529516, -0.797376, 0.289491);
@@ -126,7 +128,7 @@ public:
 		coord->SetCoordinateSystemToWorld();
 		rendw = viewport->GetRenderWindow();
 		// Connected widget signals to slots
-		// YOUR CODE HERE
+		
 		connect(but1, SIGNAL(released()), this, SLOT(load_data()));
 		connect(but2, SIGNAL(released()), this, SLOT(load_png()));
 		// Display the window
@@ -137,12 +139,13 @@ public:
 public slots: // This tells Qt we are defining slots here
 
 	// YOUR CODE HERE
+	// the load data code loads the dicom ct files
 	void load_data()
 	{
 		// read all the dicom files in the specific directory
 		reader->SetDirectoryName("C:/Users/DoseOptics/Desktop/engs_199_final_project/data/d14068-39-anon");
 		reader->Update();
-		// Image viewer
+		//Flip the dicome ct structures
 		flipYFilter->SetFilteredAxis(1);
 		flipYFilter->SetInputConnection(reader->GetOutputPort());
 		flipYFilter->Update();
@@ -150,16 +153,20 @@ public slots: // This tells Qt we are defining slots here
 		flipZFilter->SetFilteredAxis(2);
 		flipZFilter->SetInputConnection(flipYFilter->GetOutputPort());
 		flipZFilter->Update();
-
+		// setting the marching contour filter 
 		contour->SetInputConnection(flipZFilter->GetOutputPort());
 		contour->SetValue(0, -400);
 		contour->Update();
+		// get the polydata structure
 		surf = contour->GetOutput();
 		num_p = surf->GetNumberOfPoints();
+		// feed into the polydata mapper
 		mapper->SetInputConnection(contour->GetOutputPort());
 		mapper->Update();
+		// feed signal into the polydata maper
 		actor->SetMapper(mapper);
-		actor->GetProperty()->SetColor(0.0, 1.0, 1.0);
+		actor->GetProperty()->SetColor(0.5, 0.5, 0.5);
+		// connecting to the renderer and render windows.
 		ren1->SetActiveCamera(cam1);
 		rendw->AddRenderer(ren1);
 		ren1->AddActor(actor);
@@ -170,11 +177,13 @@ public slots: // This tells Qt we are defining slots here
 	void load_png()
 	{
 		// first read the image data
+		// foot print of position data for debug
 		ofstream myfile;
 		myfile.open("test.txt");
-		
+		// reading rthe png file of cherenkov images 
 		reader2->SetFileName("C:/Users/DoseOptics/Desktop/engs_199_final_project/data/185.PNG");
 		reader2->Update();
+		// save the image data into 1-D vector
 		unsigned short * image_array = static_cast<unsigned short *> (reader2->GetOutput()->GetScalarPointer());
 		vtkNew<vtkUnsignedShortArray> my_array;
 		int width = 1920, height = 1200;
@@ -183,17 +192,21 @@ public slots: // This tells Qt we are defining slots here
 		unsigned short val;
 		for (int i = 0; i < num_p; i++)
 		{
+			// get the position of point from the contour in world coodinate
 			pos = surf->GetPoint(i);
 			coord->SetValue(pos);
+			// read the position in the viewport
 			p = coord->GetComputedViewportValue(ren1);
 			if (p[0] >= 0 && p[0] <= width && p[1] >= 0 && p[1] <= height)
 				val = image_array[p[0] + p[1] * width];
 			else
 				val = 0;
+			// assign the pixel value into array. 
 			my_array->InsertNextValue(val);
 			myfile << p[0] << " " << p[1] << std::endl;
 		}
 		myfile.close();
+		// overlay the 2d pixel intensity back into the 3D contour. 
 		surf->GetPointData()->SetScalars(my_array);
 		rendw->Render();
 	}
